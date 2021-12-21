69ab5e7d41f272934c94273875d02c8e0c9b3b13
Kazuya Aoki <kazuya.aoki@kek.jp>
2021-12-22 00:09:14 +0900

    update

--- show_hist.C
+++ show_hist.C
@@ -7,7 +7,17 @@ void show_hist(const char* filename="pscan/pscan_20211102_RedFEB8.root"){
   TCanvas* c1 = new TCanvas("c1","c1",800,800);
   TFile* file = new TFile(filename);
 
-  ofstream out("trim.txt");
+  TString filename_trim = filename;
+  int ipos = filename_trim.Last('.');
+  if ( ipos >= 0 ) {
+    filename_trim.Remove(ipos);
+    filename_trim = filename_trim + "_trim.txt";
+  }else{
+    cout << "unexpected filename" << endl;
+    return;
+  }
+
+  ofstream out(filename_trim);
 
   int ch = 0;
   int ch_max = 127;
@@ -24,7 +34,9 @@ void show_hist(const char* filename="pscan/pscan_20211102_RedFEB8.root"){
   c1->Print(outputname + "[","pdf");
 
   TF1* func_calib = new TF1("func_calib","pol1");
-  func_calib->SetParameters(200,-6);
+  // 20-240. 
+  //func_calib->SetParameters(200,-6);
+  func_calib->SetParameters(240,-7.33333333);
   const double trim_amp = 18/12.5;
   
   while(ch <= ch_max){
@@ -40,8 +52,11 @@ void show_hist(const char* filename="pscan/pscan_20211102_RedFEB8.root"){
       TH1D* hist_fast = (TH1D*)file->Get(name);
       hist_fast->Fit("gaus");
       TF1* func = hist_fast->GetFunction("gaus");
-      double var = func->GetParameter(0);
-      int fast_trim = -(var-20.)*0.675 + 36;
+      int fast_trim = 36;
+      if ( func != NULL ){
+	double var = func->GetParameter(0);
+	fast_trim = -(var-20.)*0.675 + 36;
+      }
 
 
       string str;
@@ -84,8 +99,8 @@ void show_hist(const char* filename="pscan/pscan_20211102_RedFEB8.root"){
 	TString name = TString::Format("h_scurve_%d_%d",ch,d);
 	TH1D* hist = (TH1D*)file->Get(name);
 	hist->SetTitle(name);
+	c1->GetPad(itmp+1)->SetLogy();
 	hist->Draw("HIST");
-	
 	name = TString::Format("h_d_%d_%d",ch,d);
 	TH1D* histd = (TH1D*)file->Get(name);
 	histd->SetLineColor(kRed);
