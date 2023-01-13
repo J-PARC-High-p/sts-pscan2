cc0f2672599b2c474d583f72ee3a8285014b0497
Kazuya Aoki <kazuya.aoki@kek.jp>
2023-01-13 11:51:35 +0900

    wrong git com

--- amp_cal/amp_cal_manual.C
+++
@@ -1,9 +0,0 @@
-TGraph* gr;
-void amp_cal_manual()
-{
-  gr = new TGraph("amp_cal_manual.txt");
-  gr->GetXaxis()->SetTitle("amp_cal");
-  gr->GetYaxis()->SetTitle("Qin[fC]");
-  gr->SetMarkerStyle(20);
-  gr->Draw("ALP");
-}
